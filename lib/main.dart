bfa24402bc267248d3b663b195b8fc7539a46cde
Tanakan <banktv76@gmail.com>
2024-01-25 16:42:11 +0700

    setup firebase clash and analytices.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_firebase_2/app.dart';
 import 'package:flutter_bloc_firebase_2/common/config/app_configs.dart';
+import 'package:flutter_bloc_firebase_2/common/core/secure_storage/impl/secure_storage.dart';
+import 'package:flutter_bloc_firebase_2/common/core/user_session/impl/user_session.dart';
 import 'package:flutter_bloc_firebase_2/firebase_options.dart';
 import 'package:flutter_bloc_firebase_2/modules/welcome_page/views/welcome_page.dart';
 import 'package:flutter_bloc_firebase_2/router/app_route.dart';
@@ -10,7 +19,46 @@ import 'package:flutter_bloc_firebase_2/utils/app_bloc_observer.dart';
 
 void main() {}
 
-void run(Config config) async {}
+Future<void> handleError(Object error, StackTrace? stackTrace) async {
+  await FirebaseCrashlytics.instance
+      .recordError(error, stackTrace, fatal: true);
+}
+
+void run(Config config) async {
+  // Use runZonedGuarded for error handling
+  runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await EasyLocalization.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+    await FirebaseAnalytics.instance.setAnalyticsCollectionEnabled(true);
+    // Use it in Injector for use any where you want.
+    final secureStorage = SecureStorageImpl('internshipsolar');
+    final userSession = UserSessionImpl(secureStorage);
+
+    //ดังนั้น, โค้ดนี้ถูกใช้เพื่อลงทะเบียน listener
+    //ที่จะทำงานเมื่อมีข้อผิดพลาดเกิดขึ้นใน Isolate
+    //และจะนำข้อมูลข้อผิดพลาดนั้นไปให้ฟังก์ชัน handleError \
+    //ทำการจัดการ.
+
+    Isolate.current.addErrorListener(RawReceivePort((pair) async {
+      final List<dynamic> errorAndStacktrace = pair;
+      await handleError(errorAndStacktrace.first, errorAndStacktrace.last);
+    }).sendPort);
+    runApp(
+      EasyLocalization(
+        path: config.localizationPath,
+        supportedLocales: config.supportedLocales,
+        fallbackLocale: config.fallbackLocale,
+        child: const App(),
+      ),
+    );
+  }, (Object error, StackTrace stack) async {
+    await handleError(error, stack);
+  });
+}
 
 // Future main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import cloud_firestore
 import firebase_analytics
 import firebase_auth
 import firebase_core
+import firebase_crashlytics
 import flutter_secure_storage_macos
 import package_info_plus
 import path_provider_foundation
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
